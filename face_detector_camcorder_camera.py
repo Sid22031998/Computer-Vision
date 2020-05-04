c8b5f90f29b7464866e79e9c44d8ee36fa5c3574
Siddharth Singh <31348093+Sid22031998@users.noreply.github.com>
2020-05-04 13:19:50 +0530

    Add files via upload

---
+++ face_detector_camcorder_camera.py
@@ -0,0 +1,29 @@
+import cv2, time
+import numpy as np 
+
+video = cv2.VideoCapture(0)
+face_cascade = cv2.CascadeClassifier("C:\\Users\\Sid\\opencv-master\\data\\haarcascades\\haarcascade_frontalface_default.xml")
+
+a=1
+
+while True:
+	a=a+1
+	check, frame = video.read()
+	print(frame)
+	gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+	
+	faces = face_cascade.detectMultiScale(gray, scaleFactor = 1.05, minNeighbors=5)
+	
+	for x,y,w,h in faces:
+		gray  = cv2.rectangle(gray, (x,y), (x+w,y+h), (0,255,0),3)
+	
+	cv2.imshow('Capture',gray)
+	key = cv2.waitKey(1)
+	if key==ord('q'):
+		break
+
+print(a)
+
+video.release()
+
+cv2.destroyAllWindows()
